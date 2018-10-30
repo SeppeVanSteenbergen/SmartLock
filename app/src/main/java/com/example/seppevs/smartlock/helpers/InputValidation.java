e608406b567363663139977e7216e5925e1d3754
Seppe Van Steenbergen <seppevansteenbergen@hotmail.com>
2018-10-30 14:21:04 +0100

    InputValidation

--- app/src/main/java/com/example/seppevs/smartlock/helpers/InputValidation.java
+++ app/src/main/java/com/example/seppevs/smartlock/helpers/InputValidation.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
-import com.scottyab.showhidepasswordedittext.ShowHidePasswordEditText;
-
 public class InputValidation {
     private Context context;
 
